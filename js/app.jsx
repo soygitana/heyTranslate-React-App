d8adb222919988b826bdb43cdc307a2b39b2c6d5
soygitana <lizon.paula@gmail.com>
2019-09-01 16:59:46 +0200

    environment setup

---
+++ js/app.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+// import {
+//     HashRouter,
+//     Route,
+//     Link,
+//     Switch,
+//     NavLink,
+// } from 'react-router-dom';
+
+
+import Header from './components/component_header/header.jsx';
+
+class App extends Component {
+    render() {
+        return (
+            <Header />
+        )
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById("app")
+)
---
+++ js/components/component_header/header.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./header.scss";
+
+
+class Header extends Component {
+    render() {
+        return (
+            <h1>hey, i am header.</h1>
+        )
+    }
+}
+
+export default Header;
